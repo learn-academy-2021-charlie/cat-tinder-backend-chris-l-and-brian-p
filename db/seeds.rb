c152ffd607e4cf4eeb13449a53481091fe2c87fa
Chrisluna10 <chrisluna4224@gmail.com>
2021-08-25 15:48:32 -0700

    completed backend structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+characters = [
+    {
+        name: 'Freddy Krueger',
+        age: 10000,
+        enjoys: 'Nightmares'
+    },
+    {
+        name: 'Chucky',
+        age: 67,
+        enjoys: 'Toys and daycare'
+    },
+    {
+        name: 'Jigsaw',
+        age: 52,
+        enjoys: "Games"
+    }
+]
+
+characters.each do |attributes|
+    Character.create attributes
+    puts "creating character #{attributes}"
+end
