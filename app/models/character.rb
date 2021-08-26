d4a330b10f26d92275f5c9f8ca067a7954becdf4
Chrisluna10 <chrisluna4224@gmail.com>
2021-08-26 16:15:14 -0700

    fixed validations

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,4 @@
 class Character < ApplicationRecord
-    validates :name, :age, :enjoys, presence: true
+    validates :name, :age, presence: true
     validates :enjoys , presence: true, length: { minimum: 10, too_short: "minimum is 10 characters"}
 end
