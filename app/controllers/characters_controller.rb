eb2e12d2aeafb1dfa09cf4577b44eafbd4cac6c8
Bpompa95 <brianpompa02@gmail.com>
2021-08-26 11:04:58 -0700

    finish index, create

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,14 @@
 class CharactersController < ApplicationController
+    def index 
+        characters = Character.all
+        render json: characters
+    end
+    def create 
+        character = Character.create(character_params)
+        render json: character
+    end
+    private
+    def character_params
+        params.require(:character).permit(:name, :age, :enjoys)
+    end
 end
--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -2,6 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "Characters", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets the list of characters" do
+      Character.create name: 'Dracula', age: 590, enjoys: 'blood'
+
+      get '/characters'
+
+      character = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(character.length).to eq 1
+    end
   end
+  describe "POST /create " do 
+    it "creates a character" do
+      character_params = {
+      character: {
+        name: 'Dracula',
+        age: 590,
+        enjoys:'blood'
+      }
+    }
+    post '/characters', params: character_params
+    new_character = Character.first
+    expect(response).to have_http_status(200)
+    expect(new_character.name).to eq 'Dracula'
+    expect(new_character.age).to eq 590
+    expect(new_character.enjoys).to eq 'blood'
+  end
+ end
 end
